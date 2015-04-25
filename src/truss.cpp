b22859a35e0a02cc3615a27cfaa9037f48cea3d3
PyryM <pkmatikainen@gmail.com>
2015-04-24 20:47:20 -0400

    blah

--- src/truss.cpp
+++ src/truss.cpp
@@ -1,8 +1,51 @@
 // C++ truss implementation
 
+// TODO: switch to a better logging framework
+#include <iostream>
+
 using namespace trss;
 
-void Interpreter::init(trss_message* arg) {
+Interpreter::Interpreter() {
+		_thread = NULL;
+		_messageLock = SDL_CreateMutex();
+		_execLock = SDL_CreateMutex();
+		_terraState = NULL;
+		_running = false;
+		_autoExecute = true;
+		_executeOnMessage = false;
+		_executeNext = false;
+}
+
+Interpreter::~Interpreter() {
+	// Nothing special to do
+}
+
+void Interpreter::attachAddon(Addon* addon) {
+	if(!_running && _thread == NULL) {
+		_addons.push_back(addon);
+	} else {
+		std::cout << "Cannot attach addon to running interpreter.\n";
+		delete addon;
+	}
+}
+
+void Interpreter::start(trss_message* arg, const char* name) {
+	if(_thread != NULL) {
+		std::cout << "Can't start interpreter twice: already running\n"; 
+		return;
+	}
+
+	_running = true;
+	_thread = SDL_CreateThread(run_interpreter_thread, 
+								name, 
+								(void*)this);
+}
+
+void Interpreter::stop() {
+	_running = false;
+}
+
+void Interpreter::_threadEntry() {
 	_terraState = luaL_newstate();
 	luaL_openlibs(_terraState);
 	terra_Options* opts = new terra_Options;
@@ -17,5 +60,79 @@ void Interpreter::init(trss_message* arg) {
                      bootstrap->data, 
                      bootstrap->message_length, 
                      "bootstrap.t");
-	lua_pcall(_terraState, 0, 0, 0);
+	int res = lua_pcall(_terraState, 0, 0, 0);
+	if(res != 0) {
+		std::cout << "Error bootstrapping interpreter: " 
+				  << lua_tostring(_terraState, -1) << std::endl;
+	}
+
+	// Init all the addons
+	for(size_t i = 0; i < _addons.size(); ++i) {
+		_addons[i]->init(this);
+	}
+
+	// Call init
+	_safeLuaCall("_core_init");
+
+	double dt = 1.0 / 60.0; // just fudge this at the moment
+
+	// Enter thread main loop
+	while(_running) {
+		// update addons
+		for(unsigned int i = 0; i < _addons.size(); ++i) {
+			_addons[i]->update(dt);
+		}
+
+		// update lua
+		_safeLuaCall("_core_update");
+	}
+
+	// Shutdown
+	std::cout << "Shuttind down.\n";
+	// TODO: actually shutdown stuff here
+}
+
+void Interpreter::sendMessage(trss_message* message) {
+	SDL_LockMutex(_messageLock);
+	trss_acquire_message(message);
+	_curMessages.push_back(message);
+	SDL_UnlockMutex(_messageLock);
+}
+
+int Interpreter::fetchMessages() {
+	SDL_LockMutex(_messageLock);
+	// swap messages
+	std::vector<trss_message*>* temp = _curMessages;
+	_curMessages = _fetchedMessages;
+	_fetchedMessages = temp;
+
+	// clear the 'current' messages (i.e., the old fetched messages)
+	for(unsigned int i = 0; i < _curMessages.size(); ++i) {
+		trss_release_message(_curMessages[i]);
+	}
+	_curMessages.clear();
+	int numMessages = _fetchedMessages.size();
+
+	SDL_UnlockMutex(_messageLock);
+	return numMessages;
+}
+
+trss_message* Interpreter::getMessage(int index) {
+	// Note: don't need to lock because only 'our' thread
+	// should call fetchMessages (which is the only other function
+	// that touches _fetchedMessages)
+	return _fetchedMessages[index];
+}
+
+void Interpreter::_safeLuaCall(const char* funcname) {
+	lua_getglobal(_terraState, funcname);
+	int res = lua_pcall(_terraState, 0, 0, 0);
+	if(res != 0) {
+		std::cout << lua_tostring(_terraState, -1) << std::endl;
+	}
+}
+
+int run_interpreter_thread(void* interpreter) {
+	Interpreter* target = (Interpreter*)interpreter;
+	target->_threadEntry();
 }
--- src/truss.h
+++ src/truss.h
@@ -1,32 +1,76 @@
 // C++ truss header
 
 #include "trussapi.h"
+#include <string>
+#include <vector>
+
+#include "SDL_thread.h"
 
 namespace trss {
+
+	class Interpreter;
+
+	// Pure virtual class for addons
+	class Addon {
+	public:
+		virtual std::string getName() = 0;
+		virtual std::string getCHeader() = 0;
+		virtual void init(Interpreter* owner) = 0;
+		virtual void shutdown() = 0;
+		virtual void update(double dt) = 0;
+		virtual ~Addon(); // needed so it can be deleted cleanly
+	};
 
 	class Interpreter {
 	public:
 		Interpreter();
 		~Interpreter();
 
+		// the attached addon is considered to be owned by 
+		// the interpreter and will be deleted by it when the
+		// interpreter shuts down
+		void attachAddon(Addon* addon);
+
 		// Starting and stopping
-		void init(trss_message* arg);
-		void start(bool autoexecute, bool executeOnMessage);
+		void start(trss_message* arg, const char* name);
 		void stop();
 
 		// Request an execution
 		void execute();
 
-		// Messaging functions
+		// Send a message
 		void sendMessage(trss_message* message);
 		int fetchMessages();
 		trss_message* getMessage(int index);
+
+		// Inner thread
+		void _threadEntry();
 	private:
+		// Call into the actual lua/terra interpreter
+		void _safeLuaCall(const char* funcname);
+
+		// List of addons
+		std::vector<Addon*> _addons;
+
+		// Actual thread
+		SDL_Thread* _thread;
+
 		// Lock for messaging
-		mutex_t _messageLock;
+		SDL_Mutex* _messageLock;
+
+		// Messages
+		std::vector<trss_message*> _curMessages;
+		std::vector<trss_message*> _fetchedMessages;
+
+		// Lock for execution
+		// (only used if not autoexecuting)
+		SDL_Mutex* _execLock;
 
 		// Terra state
 		lua_State* _terraState;
+
+		// Whether to continue running
+		bool _running;
 
 		bool _autoExecute;
 		bool _executeOnMessage;
@@ -37,5 +81,7 @@ namespace trss {
 	public:
 	private:
 	};
+
+	static int run_interpreter_thread(void* interpreter);
 
 }
