08cfd503682906445c9cf36462b5ed96df6ab773
PyryM <pkmatikainen@gmail.com>
2015-06-11 21:11:25 -0400

    set initial script with command line argument

--- src/minimain.cpp
+++ src/minimain.cpp
@@ -6,7 +6,7 @@
 #include "wsclient_addon.h"
 #include <iostream>
 
-int main(int, char**){
+int main(int argc, char** argv){
 	trss_test();
 	trss_log(0, "Entered main!");
 	trss::Interpreter* interpreter = trss::core()->spawnInterpreter("interpreter_0");
@@ -18,6 +18,11 @@ int main(int, char**){
 	// startUnthreaded starts the interpreter in the current thread,
 	// which means the call will block until the interpreter is stopped
 	//interpreter->startUnthreaded("examples/dart_gui_test.t");
-	interpreter->startUnthreaded("examples/cam_calib_synth.t");
+	if (argc > 1) {
+		interpreter->startUnthreaded(argv[1]);
+	}
+	else {
+		interpreter->startUnthreaded("examples/cam_calib_synth.t");
+	}
 	return 0;
 }
