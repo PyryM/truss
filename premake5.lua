fc0118d275b2a9995dd0a6646a068f2a14b99963
PyryM <pkmatikainen@gmail.com>
2016-01-21 17:19:39 -0500

    premake/genie cleanup

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,18 @@
 -- premake5.lua
+
+-- paths
+BGFX_DIR  = "../bgfx"
+BX_DIR    = "../bx"
+TERRA_DIR = "../terra"
+SDL_DIR   = "../sdl" 
+
 solution "truss"
    configurations{ "Debug", "Release" }
    platforms{"x64"} -- it's 2015, 64bit is only option
    architecture "x64" -- needed in premake5 but not genie?
    location "build"
+
+   startproject "truss"
 
 project "truss"
    kind "ConsoleApp"
@@ -11,18 +20,20 @@ project "truss"
    targetdir "bin"
    location "build" 
 
-   local nvg = "extras/bgfx_nanovg/"
+   local nvg = "src/bgfx_nanovg/"
+   local ws  = "src/websocket_client/"
 
-   files{ "src/*.cpp", "src/*.h", -- core
-           nvg.."*.cpp" , nvg.."*.h"} -- nanovg
-   removefiles{nvg.."nanovg_bgfx.cpp"} -- don't compile c++ bgfx api nanovg
+   files{ "src/*.cpp", "src/*.h",                     -- core
+           nvg .. "*.cpp", nvg .. "*.h",              -- nanovg
+           ws .. "*.cpp", ws .. "*.h", ws .. "*.hpp", -- websocket
+           BGFX_DIR .. "/src/amalgamated.cpp"         -- bgfx
+         }
 
    -- link in bgfx, bx, terra, and SDL2
-   includedirs{"deps/bgfx/include", "deps/terra/include", "deps/sdl/include"}
-   includedirs{"deps/bgfx/3rdparty", "deps/bx/include"} -- extra bgfx stuff
-   libdirs{"deps/bgfx", "deps/terra", "deps/sdl/lib/x64"}
-   links{"bgfx-shared-libRelease",
-         "terra", "lua51",
+   includedirs{BGFX_DIR .. "/include", TERRA_DIR .. "/include", SDL_DIR .. "/include"}
+   includedirs{BGFX_DIR .. "/3rdparty", BX_DIR .. "/include"} -- extra bgfx stuff
+   libdirs{BGFX_DIR, TERRA_DIR, SDL_DIR .. "/lib/x64"}
+   links{"terra", "lua51",
          "SDL2", "SDL2main"}
 
    configuration "Linux"
