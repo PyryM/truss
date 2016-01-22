0f8108271a1e3771f49680a3be7fb1928339735e
PyryM <pkmatikainen@gmail.com>
2016-01-21 20:13:19 -0500

    premake build script fixes

--- premake5.lua
+++ premake5.lua
@@ -1,16 +1,18 @@
 -- premake5.lua
 
 -- paths
-BGFX_DIR  = "../bgfx"
-BX_DIR    = "../bx"
-TERRA_DIR = "../terra"
-SDL_DIR   = "../sdl" 
+BGFX_DIR   = "../bgfx"
+BX_DIR     = "../bx"
+TERRA_DIR  = "../terra"
+SDL_DIR    = "../sdl"
+PHYSFS_DIR = "../physfs"
+STB_DIR    = "../stb"
 
 solution "truss"
    configurations{ "Debug", "Release" }
    platforms{"x64"} -- it's 2015, 64bit is only option
-   architecture "x64" -- needed in premake5 but not genie?
-   location "build"
+   --architecture "x64" -- needed in premake5 but not genie?
+   location "neobuild"
 
    startproject "truss"
 
@@ -18,23 +20,26 @@ project "truss"
    kind "ConsoleApp"
    language "C++"
    targetdir "bin"
-   location "build" 
+   location "neobuild" 
 
-   local nvg = "src/bgfx_nanovg/"
-   local ws  = "src/websocket_client/"
+   local nvg = "src/addons/bgfx_nanovg/"
+   local ws  = "src/addons/websocket_client/"
 
    files{ "src/*.cpp", "src/*.h",                     -- core
            nvg .. "*.cpp", nvg .. "*.h",              -- nanovg
-           ws .. "*.cpp", ws .. "*.h", ws .. "*.hpp", -- websocket
-           BGFX_DIR .. "/src/amalgamated.cpp"         -- bgfx
+           ws .. "*.cpp", ws .. "*.h", ws .. "*.hpp" -- websocket
          }
 
    -- link in bgfx, bx, terra, and SDL2
-   includedirs{BGFX_DIR .. "/include", TERRA_DIR .. "/include", SDL_DIR .. "/include"}
+   includedirs{BGFX_DIR .. "/include", TERRA_DIR .. "/include/terra", SDL_DIR .. "/include"}
    includedirs{BGFX_DIR .. "/3rdparty", BX_DIR .. "/include"} -- extra bgfx stuff
-   libdirs{BGFX_DIR, TERRA_DIR, SDL_DIR .. "/lib/x64"}
+   includedirs{PHYSFS_DIR, STB_DIR}                           -- other includes
+   libdirs{BGFX_DIR .. "/lib", TERRA_DIR .. "/lib", SDL_DIR .. "/lib/x64", PHYSFS_DIR .. "/lib"}
    links{"terra", "lua51",
-         "SDL2", "SDL2main"}
+         "SDL2", "SDL2main",
+         "bgfx-shared-libRelease",
+         "physfs"
+         }
 
    configuration "Linux"
       links{"GL", "GLU", "X11"} -- otherwise linker will complain that bgfx is missing things
