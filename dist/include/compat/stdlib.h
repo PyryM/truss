842333f1852551d113c948a704465cd35405d9f4
Pras Velagapudi <psigen@gmail.com>
2016-09-25 17:31:22 -0400

    Added some random fixes for compatibility headers.

--- dist/include/compat/stdlib.h
+++ dist/include/compat/stdlib.h
@@ -6,12 +6,15 @@
 #ifndef STDLIB_H
 #define STDLIB_H
 
- /*  
+ /*
  *  Used by truss to check whether this header has been included from fakestd
  *  or from the actual system includes
  */
 #define TRUSS_CHECK 7777
 
 typedef unsigned long long size_t;
+
+void* malloc (size_t size);
+void free (void* ptr);
 
 #endif /* STDLIB_H */
