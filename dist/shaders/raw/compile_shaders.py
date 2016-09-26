dd41e66f6afadd1510dc885f88da29d0da6ed84e
Pyry Matikainen <pkmatikainen@gmail.com>
2016-09-25 21:24:26 -0400

    make compile_shaders.py run on posix systems

--- dist/shaders/raw/compile_shaders.py
+++ dist/shaders/raw/compile_shaders.py
@@ -5,6 +5,14 @@
 
 import sys, os
 from os.path import join, isfile
+import errno
+
+def make_sure_path_exists(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
 
 dx11cmd_fs = "shadercRelease -f {} -o {} --type f -i common\ --platform windows -p ps_4_0 -O 3"
 dx11cmd_vs = "shadercRelease -f {} -o {} --type v -i common\ --platform windows -p vs_4_0 -O 3"
@@ -15,8 +23,17 @@ dx9cmd_vs = "shadercRelease -f {} -o {} --type v -i common\ --platform windows -
 glcmd_fs = "shadercRelease -f {} -o {} --type f -i common\ --platform linux -p 120"
 glcmd_vs = "shadercRelease -f {} -o {} --type v -i common\ --platform linux -p 120"
 
-cmds_vs = [(dx11cmd_vs, "dx11"), (dx9cmd_vs, "dx9"), (glcmd_vs, "glsl")]
-cmds_fs = [(dx11cmd_fs, "dx11"), (dx9cmd_fs, "dx9"), (glcmd_fs, "glsl")]
+if os.name == "posix":
+    cmds_vs = [("./" + glcmd_vs, "glsl")]
+    cmds_fs = [("./" + glcmd_fs, "glsl")]
+else:
+    cmds_vs = [(dx11cmd_vs, "dx11"), (dx9cmd_vs, "dx9"), (glcmd_vs, "glsl")]
+    cmds_fs = [(dx11cmd_fs, "dx11"), (dx9cmd_fs, "dx9"), (glcmd_fs, "glsl")]
+
+def make_directories():
+    for (cmd, desttype) in cmds_vs:
+        print("Making sure directory " + desttype + " exists.")
+        make_sure_path_exists(join("..", desttype))
 
 def processFile(prefix, dirname, filename):
     print("Processing {}, {}, {}".format(prefix, dirname, filename))
@@ -46,6 +63,7 @@ def listfiles(dirname):
     return [ f for f in allthings if isfile(join(dirname, f)) ]
 
 def main():
+    make_directories()
     dirs = listdirs(".")
     for dirname in dirs:
         files = listfiles(dirname)
