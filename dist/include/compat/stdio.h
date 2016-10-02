dde966a9485452bde58b9430d9d9c9d99ceae88e
Pras Velagapudi <psigen@gmail.com>
2016-10-02 14:50:49 -0400

    Cleaned up headers cross-platform. (#33)
    * Standardized header guards in `compat`
    * Resolved `stddef.h` issue in #28.

--- dist/include/compat/stdio.h
+++ dist/include/compat/stdio.h
@@ -2,13 +2,18 @@
  * 'Fake' stdio.h to be included by terra on windows systems which don't
  * have visual studio installed
  */
+#ifndef TRUSS_STDIO_H
+#define TRUSS_STDIO_H
 
-#ifndef STDIO_H
-#define STDIO_H
+/*
+ * Used by truss to check whether this header has been included from compat
+ * or from the actual system includes
+ */
+#define TRUSS_CHECK_STDIO 7777
 
 /* We only use sprintf and sscanf out of stdio */
 
-int	 sprintf(char *, const char *, ...);
-int	 sscanf(const char *, const char *, ...);
+int sprintf(char *, const char *, ...);
+int sscanf(const char *, const char *, ...);
 
-#endif /* STDIO_H */
+#endif /* TRUSS_STDIO_H */
--- dist/include/compat/stdlib.h
+++ dist/include/compat/stdlib.h
@@ -2,19 +2,18 @@
  * 'Fake' stdlib.h to be included by terra on windows systems which don't
  * have visual studio installed
  */
+#ifndef TRUSS_STDLIB_H
+#define TRUSS_STDLIB_H
 
-#ifndef STDLIB_H
-#define STDLIB_H
-
- /*
- *  Used by truss to check whether this header has been included from fakestd
- *  or from the actual system includes
+/*
+ * Used by truss to check whether this header has been included from fakestd
+ * or from the actual system includes
  */
-#define TRUSS_CHECK 7777
+#define TRUSS_CHECK_STDLIB 7777
 
-typedef unsigned long long size_t;
+#include <stddef.h>
 
-void* malloc (size_t size);
-void free (void* ptr);
+void* malloc(size_t size);
+void free(void* ptr);
 
-#endif /* STDLIB_H */
+#endif /* TRUSS_STDLIB_H */
