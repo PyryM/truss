2bc87e1519b70f6012897644e7d5fb514f7edf14
Pras Velagapudi <psigen@gmail.com>
2017-12-01 21:28:43 -0500

    Added correct scrolling!

--- docs/js/sidebar.js
+++ docs/js/sidebar.js
@@ -1,6 +1,10 @@
 const docLinkSelector = '.docs-link';
+const docSublinkSelector = '.docs-sublink';
 const docContentSelector = '#content';
-const titleBase = 'TRUSS - ';
+const titleBase = document.title + ' - ';
+
+let $currentHeader = $();
+let $currentSubheader = $();
 
 // Highlight the current section of the documentation that we are in.
 jQuery(function() {
@@ -9,30 +13,85 @@ jQuery(function() {
     .attr('href');
 
   if (docFirstHref !== undefined && docFirstHref.charAt(0) === '#') {
+    addSublinks();
+
     setActiveSidebarLink();
+    setActiveSidebarSublink($currentHeader);
 
     $(window).on('scroll', function(evt) {
       setActiveSidebarLink();
+      setActiveSidebarSublink($currentHeader);
     });
   }
 });
 
-// Modifies the class on closest menu header to the current scroll location.
-function setActiveSidebarLink() {
-  $(docLinkSelector).removeClass('active');
-  const $closest = getClosestHeader();
-  $closest.addClass('active');
-  document.title = titleBase + $closest.text();
+// Creates menu entries containing subheaders for entire document.
+function addSublinks() {
+  const $links = $(docLinkSelector);
+  $links.each(function() {
+    const $heading = $(this);
+    const $subheadings = $($heading.attr('href'))
+      .closest('.ui.vertical.segment')
+      .children('h2');
+
+    const $subheadingMenu = $('<div class="menu" />').appendTo($heading);
+    $subheadings.each(function() {
+      $subheadingMenu.append(
+        `<a class="ui item docs-sublink" href="#${this.id}">${$(
+          this,
+        ).text()}</div>`,
+      );
+    });
+  });
 }
 
-// Looks for a link that contains the `docs-link` class and uses the position
-// of its reference to determine if it is the closest header to the user.
-function getClosestHeader() {
-  const $links = $(docLinkSelector);
+// Modifies the class on closest menu header to the current scroll location.
+function setActiveSidebarLink() {
+  const $closest = getClosestHeader(docLinkSelector);
+
+  if ($currentHeader === $closest) return;
+  $currentHeader = $closest;
+
+  $(docLinkSelector).removeClass('active');
+  $(docLinkSelector)
+    .children('.menu')
+    .hide();
+
+  $closest.addClass('active');
+  $closest.children('.menu').show();
+
+  document.title =
+    titleBase +
+    $closest
+      .clone()
+      .children()
+      .remove()
+      .end()
+      .text();
+}
+
+// Modifies the class on closest secondary header for current scroll location.
+// We include the main header such that if it is the closest, no subheader will
+// be selected at all.
+function setActiveSidebarSublink($heading) {
+  const $subheadings = $heading.find(docSublinkSelector).add($heading);
+  const $closest = getClosestHeader($subheadings);
+
+  if ($currentSubheader === $closest) return;
+  $currentSubheader = $closest;
+
+  $(docSublinkSelector).removeClass('active');
+  $closest.addClass('active');
+}
+
+// Looks for a link that contains the specified selector and uses the position
+// of its reference to determine if it is the closest anchor to the user.
+function getClosestHeader(selector) {
+  const $links = $(selector);
   const top = window.scrollY;
   let $last = $links.first();
 
-  if (top < 300) {
+  if (top < 200) {
     return $last;
   }
 
@@ -40,17 +99,17 @@ function getClosestHeader() {
     return $links.last();
   }
 
-  for (var i = 0; i < $links.length; i++) {
-    var $link = $links.eq(i),
-      href = $link.attr('href');
+  for (let i = 0; i < $links.length; i++) {
+    const $link = $links.eq(i);
+    const href = $link.attr('href');
 
     if (href !== undefined && href.charAt(0) === '#' && href.length > 1) {
-      var $anchor = $(href);
+      const $anchor = $(href);
 
       if ($anchor.length > 0) {
-        var offset = $anchor.offset();
+        const offset = $anchor.offset();
 
-        if (top < offset.top - 300) {
+        if (top < offset.top - 200) {
           return $last;
         }
 
